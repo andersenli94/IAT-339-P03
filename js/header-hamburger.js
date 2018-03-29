86e44fb7a079a163451f88f12c4b6739848285d5
Unknown <andersenli94@gmail.com>
2018-03-29 12:02:30 -0700

    updated responsiveness

--- js/header-hamburger.js
+++ js/header-hamburger.js
@@ -5,62 +5,49 @@ console.log("script has started running");
 var windowWidth = window.innerWidth;
 
 
-var hamburgerMenu = document.querySelector(".hamburger-menu");
-var navItems = document.querySelector("#main-nav-items");
-var sideNav = document.querySelector(".side-nav");
-var sideNavCloseButton = document.querySelector("#side-nav-close-button");
+var hamburgerMenu = document.querySelector("#menu-toggle");
+var overlay = document.querySelector(".overlay");
 
-var collapseWidth = 1100;
+var menuOpen = false;
+
+var collapseWidth = 1000;
 
 if (windowWidth < collapseWidth){
 	console.log("windowWidth < " + collapseWidth + "; collapsing menu");
 
-	centerNavToggle.classList.remove("hidden");
-	centerNavToggle.setAttribute("aria-hidden", "false");
-
-
-	navItems.classList.add("hidden");
-	navItems.setAttribute("aria-hidden", "true");
-
-  sideNavCloseButton.classList.remove("hidden");
-  sideNavCloseButton.setAttribute("aria-hidden", "false");
-
-	centerNavToggle.addEventListener("click",
+	hamburgerMenu.addEventListener("click",
 		function(){
-			console.log("#nav-toggle has been clicked.");
+			console.log("menu icon clicked");
 
-			if(navItems.classList.contains("hidden")){
-				console.log("#nav-items hidden, now showing.");
-				navItems.classList.remove("hidden");
-        navItems.setAttribute("aria-hidden", "false");
-        sideNavCloseButton.classList.remove("hidden");
-        sideNavCloseButton.setAttribute("aria-hidden", "false");
-        sideNav.style.display = "block";
 
+			console.log("#nav-items hidden, now showing.");
+      if (menuOpen == false){
+				overlay.style.height = '100%';
+				menuOpen = true;
 			}
-			else{
-				console.log("#nav-items showing, now hiding.");
-				navItems.classList.add("hidden");
-        navItems.setAttribute("aria-hidden", "true");
+			else if (menuOpen == true){
+				overlay.style.height = '0%';
+				menuOpen = false;
 			}
+
 		})
 
-  sideNavCloseButton.addEventListener("click",
-    function () {
-      if(navItems.classList.contains("hidden")){
-        console.log("#nav-items hidden, now showing.");
-        navItems.classList.remove("hidden");
-        navItems.setAttribute("aria-hidden", "false");
-
-      }
-      else{
-        console.log("#nav-items showing, now hiding.");
-        navItems.classList.add("hidden");
-        navItems.setAttribute("aria-hidden", "true");
-        sideNavCloseButton.classList.add("hidden");
-        sideNavCloseButton.setAttribute("aria-hidden", "true");
-        sideNav.style.display = "none";
-      }
-    });
+  // sideNavCloseButton.addEventListener("click",
+  //   function () {
+  //     if(navItems.classList.contains("hidden")){
+  //       console.log("#nav-items hidden, now showing.");
+  //       navItems.classList.remove("hidden");
+  //       navItems.setAttribute("aria-hidden", "false");
+	//
+  //     }
+  //     else{
+  //       console.log("#nav-items showing, now hiding.");
+  //       navItems.classList.add("hidden");
+  //       navItems.setAttribute("aria-hidden", "true");
+  //       sideNavCloseButton.classList.add("hidden");
+  //       sideNavCloseButton.setAttribute("aria-hidden", "true");
+  //       sideNav.style.display = "none";
+  //     }
+  //   });
 
 }
